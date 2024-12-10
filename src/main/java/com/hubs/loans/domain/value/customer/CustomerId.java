ec7fb5b0c2e22e4e7ca547fee9d399f714f9946a
gokhanustuner <gokhanustuner@gmail.com>
2024-12-10 21:39:50 +0300

    Remove serializable implementation from CustomerId value object

--- src/main/java/com/hubs/loans/domain/value/customer/CustomerId.java
+++ src/main/java/com/hubs/loans/domain/value/customer/CustomerId.java
@@ -1,8 +1,7 @@
 package com.hubs.loans.domain.value.customer;
 
-import java.io.Serializable;
 import java.util.UUID;
 
 
-public record CustomerId(UUID id) implements Serializable {
+public record CustomerId(UUID id) {
 }
