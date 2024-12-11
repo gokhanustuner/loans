29f53a34c87f07aabc097672493374a8be9e2abc
gokhanustuner <gokhanustuner@gmail.com>
2024-12-11 03:51:59 +0300

    Fix in LoanAmountConverter according to domain validation of values

--- src/main/java/com/hubs/loans/infrastructure/converter/LoanAmountConverter.java
+++ src/main/java/com/hubs/loans/infrastructure/converter/LoanAmountConverter.java
@@ -1,6 +1,5 @@
 package com.hubs.loans.infrastructure.converter;
 
-import com.hubs.loans.domain.value.loan.InterestRate;
 import com.hubs.loans.domain.value.loan.LoanAmount;
 import jakarta.persistence.AttributeConverter;
 import jakarta.persistence.Converter;
@@ -11,13 +10,11 @@ import java.math.BigDecimal;
 public class LoanAmountConverter implements AttributeConverter<LoanAmount, BigDecimal> {
     @Override
     public BigDecimal convertToDatabaseColumn(LoanAmount loanAmount) {
-        System.out.println("LoanAmountConverter::convertToDatabaseColumn");
         return loanAmount.amount();
     }
 
     @Override
     public LoanAmount convertToEntityAttribute(BigDecimal loanAmount) {
-        System.out.println("LoanAmountConverter::convertToEntityAttribute");
-        return new LoanAmount(BigDecimal.ZERO, loanAmount);
+        return new LoanAmount(LoanAmount.MIN, loanAmount);
     }
 }
