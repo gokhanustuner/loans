6a88e22f4a06732f940302bffb8497a3b48ebddb
gokhanustuner <gokhanustuner@gmail.com>
2024-12-12 17:56:50 +0300

    LoanFactory test for installment placement and loan creation

--- src/test/java/com/hubs/loans/domain/factory/LoanFactoryTest.java
+++ src/test/java/com/hubs/loans/domain/factory/LoanFactoryTest.java
@@ -1,17 +1,24 @@
 package com.hubs.loans.domain.factory;
 
 import com.hubs.loans.domain.entity.Customer;
+import com.hubs.loans.domain.entity.Installment;
 import com.hubs.loans.domain.entity.Loan;
 import com.hubs.loans.domain.value.customer.CreditLimit;
+import com.hubs.loans.domain.value.installment.NumberOfInstallments;
+import com.hubs.loans.domain.value.loan.InterestRate;
+import com.hubs.loans.domain.value.loan.LoanAmount;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.math.BigDecimal;
+import java.util.List;
 import java.util.UUID;
 
 import static org.mockito.Mockito.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 @ExtendWith(MockitoExtension.class)
 public class LoanFactoryTest {
@@ -22,7 +29,8 @@ public class LoanFactoryTest {
     @Mock
     private InstallmentsFactory installmentsFactory;
 
-    public void loan_factory_creates_loan_as_expected_when_rules_of_creation_is_ok() {
+    @Test
+    public void loan_factory_creates_loan_and_places_its_installments() {
         Customer customer =
                 new Customer(
                         UUID.randomUUID(),
@@ -31,7 +39,15 @@ public class LoanFactoryTest {
                         new CreditLimit(BigDecimal.ZERO, BigDecimal.valueOf(198000))
                 );
 
+        List<Installment> installmentList = List.of(new Installment(), new Installment());
+        when(installmentsFactory.createInstallmentsWithLoan(any(Loan.class))).thenReturn(installmentList);
 
-        //when(installmentsFactory.createInstallmentsWithLoan(customer.makeLoan()))
+        Loan loan = loanFactory.createLoanWithCustomer(
+                customer,
+                LoanAmount.of(LoanAmount.MIN, new InterestRate(0.3)),
+                new NumberOfInstallments(6)
+        );
+
+        assertArrayEquals(installmentList.toArray(), loan.getInstallments().toArray());
     }
 }
