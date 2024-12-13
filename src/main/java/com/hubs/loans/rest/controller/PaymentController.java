12ccc6a40aacbf309c5726f9e434fcdccc4d65b9
gokhanustuner <gokhanustuner@gmail.com>
2024-12-13 07:22:17 +0300

    Fix RequestMapping

--- src/main/java/com/hubs/loans/rest/controller/PaymentController.java
+++ src/main/java/com/hubs/loans/rest/controller/PaymentController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.UUID;
 
 @RestController
-@RequestMapping("/payment")
+@RequestMapping("/api/payment")
 @RequiredArgsConstructor
 public class PaymentController {
 
